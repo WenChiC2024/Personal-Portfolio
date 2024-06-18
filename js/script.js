076be3b0390a28e1bccf6d1f32e45bc664853ab5
WenChiC2024 <wenchic@uw.edu>
2024-06-17 23:07:25 -0700

    Updated Resume

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Analyst'],
+    strings: ['Data Scientist', 'Software Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
